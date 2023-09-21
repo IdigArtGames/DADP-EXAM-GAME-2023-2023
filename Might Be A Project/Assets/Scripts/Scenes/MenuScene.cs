4bbddc9f6bb006e85511be897f53e73560c07090
IdigArtGames <132227781+IdigArtGames@users.noreply.github.com>
2023-09-21 15:12:03 +0200

    DADP

---
+++ Might Be A Project/Assets/Scripts/Scenes/MenuScene.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
